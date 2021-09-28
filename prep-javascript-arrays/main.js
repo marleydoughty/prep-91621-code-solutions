4a56ecae0d0cdc583ecf4d9d765e79bc4502754f
Marley Doughty <marleydoughty@yahoo.com>
2021-09-27 18:50:00 -0700

    completed prep-javascript-arrays

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,16 @@
+var languages;
+languages = ['html', 'css',
+  'javascript',
+  'react',
+  'node',
+  'postgres'];
+console.log(languages);
+var firstElement;
+firstElement = languages[0];
+console.log('The first element of the array is', firstElement);
+var thirdElement = languages[2];
+console.log('The third element of the array is', thirdElement);
+var length = languages.length;
+console.log('The length of this array is', length);
+var lastElement = languages[languages.length - 1];
+console.log('The last element of the array is', lastElement);
